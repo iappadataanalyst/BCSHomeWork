2f173e01669ea74d4d10b67644a97d5c4aa9461b
iAppa <iappadataanalyst@gmail.com>
2020-01-28 13:35:07 -0500

    API Homework

--- Homework6/python-api-challenge_iAPPA/starter_code/api_keys.py
+++ Homework6/python-api-challenge_iAPPA/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6b7ff1f266cc9acdb5c52d64e0826698"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBwiWBdcMksrxnWzcOvCM1cjxhqV8017_A"
